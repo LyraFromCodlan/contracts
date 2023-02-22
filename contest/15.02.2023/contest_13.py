b4d73c8b8fdd9fe4c1bfa23fce764a0cec80fe6b
LyraFromCodlan <nurtaytulegenov@gmail.com>
2023-02-22 16:48:44 +0600

    Added solution and corresponding test case for contest_13

--- contest/15.02.2023/contest_13.py
+++ contest/15.02.2023/contest_13.py
@@ -7,13 +7,18 @@ ops = {
 
 with open("input.txt","r") as ifile, open("output.txt", "w") as ofile:
     equation : list =[el for el in ifile.readline().strip().split()]
+    stack = list()
     ind : int = 0
     while ind <len(equation):
         if equation[ind] in ['+','-','*']:
-            equation[ind-2] = ops[equation[ind]](int(equation[ind-1]),int(equation[ind-2]))
-            ind -=2
-            equation.pop(ind+1)
-            equation.pop(ind+1)
+            if len(stack)==1 and equation[ind]=='-':
+                result = 0-stack[-1]
+                stack[-1]=result
+            else:
+                result = ops[equation[ind]](stack[-2],stack[-1])
+                stack.pop(-1)
+                stack[-1]=result
+        else:
+            stack.append(int(equation[ind]))
         ind += 1
-    ofile.write(str(equation)[1:-1]+"\n")
-    
+    ofile.write(str(stack[0])+"\n")
